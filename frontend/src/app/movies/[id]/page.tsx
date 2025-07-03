89916274ec02aa0324ae83b7e15ffdfad403fe8a
StilyanI <stilyan.i.ivanov@gmail.com>
2025-07-03 13:31:29 +0300

    WIP Movie page

--- frontend/src/app/movies/[id]/page.tsx
+++ frontend/src/app/movies/[id]/page.tsx
@@ -1,3 +1,43 @@
+'use client'
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+//testing data
+const td = { 
+  title: "F1",
+  year: 2025,
+  cast: ["Brad Pitt", "Damson Idris"],
+  poster: 'https://a.ltrbxd.com/resized/film-poster/8/1/7/9/7/7/817977-f1-the-movie-0-1000-0-1500-crop.jpg?v=f5ae2b99b9',
+  summary: "Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory."
+}
+
+
 export default function MovieDetails() {
-  return <h1>Movie with id 1</h1>;
+  const pn = usePathname()
+  const id = pn.substring(pn.lastIndexOf('/') + 1);
+  useEffect(() => {
+    console.log(id);
+  })
+
+  const renderCast = () => {
+  return td.cast.map((castMember) => {
+    return <Typography
+      sx = {{background: "gray", borderRadius: "3px", display: "inline-block", padding: 1, color: "white"}}
+    >{castMember}</Typography>
+  })
+}
+
+  return(
+    <Container>
+      <Typography
+        variant="h2"
+      >{td.title}</Typography>
+      <Typography>{td.year}</Typography>
+      <Typography>{td.summary}</Typography>
+      <Grid container spacing={1}>
+        {renderCast()}
+      </Grid>
+    </Container>
+  )
 }
