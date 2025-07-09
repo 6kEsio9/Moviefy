6c2f324f3093078be972f9eff0186d7f680da2a4
StilyanI <stilyan.i.ivanov@gmail.com>
2025-07-09 11:50:55 +0300

    made watchilst buttons work with state

--- frontend/src/app/components/MovieID/WatchlistButtons.tsx
+++ frontend/src/app/components/MovieID/WatchlistButtons.tsx
@@ -1,34 +1,85 @@
-import { List, RemoveRedEyeRounded } from "@mui/icons-material";
+import { useAuth } from "@/app/hooks/useAuth";
+import { User } from "@/app/services/AuthService";
+import { AccessTime, List, RemoveRedEyeRounded } from "@mui/icons-material";
 import { IconButton, Typography } from "@mui/material";
-import { useState } from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function WatchlistButtons(){
-  const [watchStatus, setWatchStatus] = useState('notwatched');
+  const {user, setUser} = useAuth();
+  const movieId = (+useParams().id!);
+  const [watchStatus, setWatchStatus] = useState('none');
 
-  const addToWatched = () => {
-    if(watchStatus === "watched"){
-      setWatchStatus("notwatched");
-    } else {
-      setWatchStatus("watched")
+  const handleWatchlistChange = (state: string) => {
+    let updatedWatched = user?.watchList.watched;
+    let updatedPlan = user?.watchList.willWatch;
+    let updatedWatching = user?.watchList.isWatching;
+
+    switch(state){
+      case "watched": {
+        if(watchStatus === "watched"){
+          setWatchStatus("none");
+          updatedWatched = updatedWatched!.filter((x) => x !== movieId)
+        }else{
+          setWatchStatus("watched");
+          updatedWatched?.push(movieId);
+          updatedWatching = updatedWatching?.filter((x) => x !== movieId)
+          updatedPlan = updatedPlan?.filter((x) => x !== movieId)
+        }
+        break;
+      }
+
+      case "plan": {
+        if(watchStatus === "plan"){
+          setWatchStatus("none")
+          updatedPlan = updatedPlan!.filter((x) => x !== movieId)
+        }else{
+          setWatchStatus("plan")
+          updatedPlan?.push(movieId)
+          updatedWatched = updatedWatched!.filter((x) => x !== movieId)
+          updatedWatching = updatedWatching?.filter((x) => x !== movieId)
+        }
+        break;
+      }
+
+      case "watching": {
+        if(watchStatus === "watching"){
+          setWatchStatus("none")
+          updatedWatching = updatedWatching!.filter((x) => x !== movieId)
+        }else{
+          setWatchStatus("watching")
+          updatedWatching?.push(movieId)
+          updatedWatched = updatedWatched!.filter((x) => x !== movieId)
+          updatedPlan = updatedPlan!.filter((x) => x !== movieId)
+        }
+        break;
+      }
     }
+
+    const updatedWatchlist = {watched: updatedWatched!, isWatching: updatedWatching!, willWatch: updatedPlan!}
+    const updatedUser = {...user, watchList: updatedWatchlist};
+    setUser(updatedUser as User);
   }
 
-  const addToPlanToWatch = () => {
-    if(watchStatus === "plan"){
-      setWatchStatus("notwatched");
-    } else {
-      setWatchStatus("plan")
-    }
-  }
+  useEffect(() => {
+    if(user?.watchList.watched.includes(+movieId!))setWatchStatus("watched")
+      else if(user?.watchList.willWatch.includes(+movieId!))setWatchStatus("plan")
+        else if(user?.watchList.willWatch.includes(+movieId!))setWatchStatus("watching");
+          else setWatchStatus('none')
+  }, [])
 
   return(
   <div style={{position: "absolute", right: "20%"}}>
-    <IconButton onClick={addToWatched} color={watchStatus === "watched" ? "success" : "default"}>
+    <IconButton onClick={() => handleWatchlistChange("watched")} color={watchStatus === "watched" ? "success" : "default"}>
       <RemoveRedEyeRounded/>
       <Typography>{watchStatus === "watched" ? "Watched" : "Watch"}</Typography>
     </IconButton>
-    <IconButton onClick={addToPlanToWatch} color={watchStatus === "plan" ? "secondary" : "default"}>
+    <IconButton onClick={() => handleWatchlistChange("watching")} color={watchStatus === "watching" ? "info" : "default"}>
       <List/>
+      <Typography>{watchStatus === "watching" ? "Currently watching" : "Watching"}</Typography>
+    </IconButton>
+    <IconButton onClick={() => handleWatchlistChange("plan")} color={watchStatus === "plan" ? "secondary" : "default"}>
+      <AccessTime/>
       <Typography>{watchStatus === "plan" ? "Planned to watch" : "Plan to watch"}</Typography>
     </IconButton>
   </div>
